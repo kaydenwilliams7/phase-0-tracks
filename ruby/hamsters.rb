15b12c62ffedc6fab12b178e91adcefb1e8ec5fd
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-05-27 18:24:04 -0700

    Added integers, booleans and floats

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,11 +1,20 @@
 puts "What is the Hamster's name?"
-name = gets.chomp
+	name = gets.chomp
 puts "What is the volume level of the hamster?"
-volume_level = gets.chomp
+	volume_level = gets.chomp.to_f
+
 puts "What is the fur color?"
-fur_color = gets.chomp
+	fur_color = gets.chomp
+
 puts "Is it a good candidate for adoption?"
-good_candidate = gets.chomp
+	good_candidate = gets.chomp
+	if good_candidate == "yes"
+	   good_candidate = true
+	else 
+		good_candidate = false
+	end
+	
+
 puts "How old is the hamster?"
-estimated_age = gets.chomp
+	estimated_age = gets.chomp.to_i
 
