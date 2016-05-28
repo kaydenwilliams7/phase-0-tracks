c34829fbaf9d8f6d3e67740275c1b0102d981c76
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-05-27 18:03:14 -0700

    Added questions and variables to form

---
+++ ruby/hamsters.rb
@@ -0,0 +1,11 @@
+puts "What is the Hamster's name?"
+name = gets.chomp
+puts "What is the volume level of the hamster?"
+volume_level = gets.chomp
+puts "What is the fur color?"
+fur_color = gets.chomp
+puts "Is it a good candidate for adoption?"
+good_candidate = gets.chomp
+puts "How old is the hamster?"
+estimated_age = gets.chomp
+
