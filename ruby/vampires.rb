d6bb12c89edc8b601077f3688e05428980d46246
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-05-30 04:55:13 -0700

    Added vampire detection questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,4 @@
+puts "What is your name?"
+puts "How old are you? What year were you born?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Would you like to enroll in the company’s health insurance?"
