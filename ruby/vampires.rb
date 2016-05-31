16a826742d2afe6e198ee54f4292f020e776338c
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-05-31 04:06:39 -0700

    Added if and elsif statments for vampire answers

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,22 @@
 def vampire_dection()
+	likelyhood_vampire = 0
 	puts "What is your name?"
 	name = gets.chomp
 	if name == "Drake Cula" || "Tu Fang"
-		return "Definitely a vampire!"		
+		return "Definitely a vampire"		
 	end
 	puts "How old are you? What year were you born?"
+	stated_age = gets.chomp.to_i
+	stated_year_born = gets.chomp.to_i
 	
 	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-	
+	garlic_prefrences = gets.chomp.downcase 
 	puts "Would you like to enroll in the company’s health insurance?"
+	enrollment_prefrences = gets.chomp.downcase
+	if (stated_age == 2016 - stated_year_born) && (garlic_prefrences == "yes" || "yes")
+		puts "Probably not a vampire."
+	elsif (stated_age != 2016 - stated_year_born) && (garlic_prefrences == "no" || enrollment_prefrences == "no")
+			
+	end 
 end
 
