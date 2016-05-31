f4a1ca45fd517402aec5d42a25dd518277cb3ed8
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-05-31 03:44:51 -0700

    Added first question in vampire dection method

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,13 @@
-puts "What is your name?"
-puts "How old are you? What year were you born?"
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-puts "Would you like to enroll in the company’s health insurance?"
+def vampire_dection()
+	puts "What is your name?"
+	name = gets.chomp
+	if name == "Drake Cula" || "Tu Fang"
+		return "Definitely a vampire!"		
+	end
+	puts "How old are you? What year were you born?"
+	
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	
+	puts "Would you like to enroll in the company’s health insurance?"
+end
+
