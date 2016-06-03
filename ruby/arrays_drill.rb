b5dc439ca2fa9bc2c120fc9247c2194b37b49116
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-03 14:17:17 -0700

    Initial commit from Catlinlikesrobots

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,22 @@
+new_array = []
+p new_array
+
+new_array << "J.K. Rowling" << "George R. R. Martin" << "Suzanne Collins" << "Stephanie Meyer" << "J. R. R. Tolkein"
+p new_array
+
+# new_array.delete_at(2)
+# p new_array
+
+# new_array.insert(2, "Brian Sanderson")
+# p new_array
+
+new_array.delete("J.K. Rowling")
+p new_array
+
+p "It is #{new_array.include?("T.S. Eliot")} that T.S. Eliot is in new_array."
+
+second_array = ["Anne Carson", "Walt Whitman", "Shakespeare"]
+
+final_array = new_array << second_array
+
+p final_array
