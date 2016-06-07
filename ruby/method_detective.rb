8993aed3037b15569d6610512a20e7f3598f0c3a
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-07 05:22:18 -0700

    added all methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -10,30 +10,33 @@
  "zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".center(15)
+ "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+ "the usual" + " suspects"
+ "the usual".concat( "suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+ " suspects".replace("the usual suspects")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".slice(0..38)
+ "The case of the disappearing last letter".replace("The case of the disappearing last lette")
+
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".slice(1..39)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+ "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".codepoints
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
