ae2942d85dc488afe7740d58d2d2347a14d11f01
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-09 06:13:44 -0700

    Added keys to hash and ask user for input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -12,3 +12,39 @@ Pseudocode:
 -Create loop to ask each client for the various info
 -Thank the client for filling out the form.
 =end
+
+def client_info_form
+	applicant = {}
+
+	puts "What is your name?"
+	applicant[:name] = gets.chomp
+
+	puts "How old are you?"
+	applicant[:age] = gets.chomp.to_i
+
+	puts "What is your address?"
+	applicant[:address] = gets.chomp
+
+	puts "How large is your household?"
+	applicant[:household_number] = gets.chomp.to_i
+
+	puts "What decor do you prefer?"
+	applicant[:decor_theme] = gets.chomp
+
+	puts "What is your favorite color?"
+	applicant[:favorite_color] = gets.chomp
+
+	puts "Do you have any pets?"
+	if gets.chomp == "yes"
+		applicant[:pets] = true
+	else
+		applicant[:pets] = false
+	end		
+	 
+
+end
+
+
+
+
+
