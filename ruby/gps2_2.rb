b2ce2edb148da43d85761191b2ce7ab9c5371287
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-09 20:55:31 -0700

    Added pcode for create list method

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,43 @@
+=begin
+Method to create a list
+	-define #create_list
+
+input: string of items separated by spaces (example: "carrots apples cereal pizza")
+steps: 
+  -Create a keys based on the string input
+  -Value of keys would be the quantity needed of each key.
+  set default quantity
+  print the list to the console [can you use one of your other methods here?]
+  -call #print_method 
+output: [what data type goes here, array or hash?]
+	-hash
+Method to add an item to a list
+input: item name and optional quantity
+steps:
+output:
+
+Method to remove an item from the list
+input:
+steps:
+output:
+
+Method to update the quantity of an item
+input:
+steps:
+output:
+
+Method to print a list and make it look pretty
+input:
+steps:
+output:
+
+=end
+
+
+
+
+
+
+
+
+
