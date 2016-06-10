3bcbbcdf62801c06f0fef5aed70e420a5c984a89
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-09 23:13:32 -0700

    Added all methods to list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -17,7 +17,7 @@ steps:
 	-turn item name input to a symbol at the end of the earlier hash
 	-quantity will be value of item name input.
 output:
-	-p udated grocery list using #print.
+	-p updated grocery list using #print.
 
 Method to remove an item from the list
 input:
@@ -26,7 +26,7 @@ steps:
 	-Delete key - value pair
 output:
 	-hash with the delete key-value pair gone.
-	-p udated grocery list using #print.
+	-p undated grocery list using #print.
 
 Method to update the quantity of an item
 input:
@@ -36,7 +36,7 @@ steps:
 	-first we need to find the key
 	-then change the value of the key to the new quantity
 output:
-	-Upadated hash with new quantity in item input.
+	-Updated hash with new quantity in item input.
 
 Method to print a list and make it look pretty
 input:
@@ -49,27 +49,42 @@ output:
 	-A prettyfied hash
 =end
 
-# Method to create a list
-#     -define #create_list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
-#   - Seperate items by spaces
-#   - Create a keys based on the string input
-#   - Value of keys would be the quantity needed of each key.
-#   - set default quantity
-#   print the list to the console [can you use one of your other methods here?]
-#   -call #print_method 
-# output: [what data type goes here, array or hash?]
-#     -hash
-​
-def create_list(str)
-    grocery_list = {}
-    grocery_items = str.split(" ")
-    grocery_items.each {|grocery_items| grocery_list[:grocery_items] = 1}
+def prettyfie_list(hash)
+	puts "This is your current grocery list:"
+	puts "\n"
+	hash.each {|key, value| puts "#{key} quantity = #{value}" }
+	puts "\n"
 end
-create_list("apples oranges")
 
+def create_list(str)
+    $grocery_list = {}
+    grocery_items = str.split(" ")
+    grocery_items.each {|grocery_items| $grocery_list[grocery_items.to_sym] = 1}
+    $grocery_list
+    prettyfie_list($grocery_list)
+end
+create_list("lemonade tomatoes onions ice_cream")
 
+def add_item(key, quantity)
+	$grocery_list[key.to_sym] = quantity
+end
+
+def remove_item(key)
+	$grocery_list.delete(key)		
+end
+
+def update_quantity(key, quantity)
+	$grocery_list[key] = quantity
+end
+
+update_quantity(:lemonade, 2)
+update_quantity(:tomatoes, 3)
+update_quantity(:ice_cream, 4)
+
+remove_item(:lemonade)
+update_quantity(:ice_cream, 1)
+
+prettyfie_list($grocery_list)
 
 
 
