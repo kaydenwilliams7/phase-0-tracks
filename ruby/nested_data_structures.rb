c113b72670683d8070bb44a57e6a86d8cc19da0a
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-09 18:53:29 -0700

    End of project 5.5

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,5 +1,9 @@
 library = {
 	:fantasy => ['Harry Potter Series', 'The Twilight Saga', 'Lorien Legacies'],
 	:finance => ['The Total Money Makeover', 'Rich Dad, Poor Dad'],
-	:sci_fi => {'Brian Sanderson' => 'The Reckoners'},
+	:sci_fi => {'Brian Sanderson' => 'The Reckoners', 'total book count' => 1050}
 }
+
+p library[:fantasy][1]
+p library[:sci_fi]['Brian Sanderson']
+p library[:sci_fi]['total book count']
