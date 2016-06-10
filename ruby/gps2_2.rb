2b5789836df52c605f064daf5255ab331085ca35
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-09 21:01:14 -0700

    Add pcode for method to remove key/value pair

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,7 +1,6 @@
 =begin
 Method to create a list
 	-define #create_list
-
 input: string of items separated by spaces (example: "carrots apples cereal pizza")
 steps: 
   -Create a keys based on the string input
@@ -11,16 +10,24 @@ steps:
   -call #print_method 
 output: [what data type goes here, array or hash?]
 	-hash
+
 Method to add an item to a list
 input: item name and optional quantity
 steps:
+	-turn item name input to a symbol at the end of the earlier hash
+	-quantity will be value of item name input.
 output:
+	-p udated grocery list using #print.
 
 Method to remove an item from the list
 input:
+	-Would be Key you are trying to delete
 steps:
+	-Delete key - value pair
 output:
-
+	-hash with the delete key-value pair gone.
+	-p udated grocery list using #print.
+	
 Method to update the quantity of an item
 input:
 steps:
