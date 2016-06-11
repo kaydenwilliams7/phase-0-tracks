906d534fef67154b10d708cb74c77d9852b65424
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-11 08:13:19 -0700

    Add space lines

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -30,7 +30,17 @@ class Santa
 
 end
 
-saint_nick = Santa.new
-saint_nick.speak
-saint_nick.eat_milk_and_cookies('cookie')
+# saint_nick = Santa.new
+# saint_nick.speak
+# saint_nick.eat_milk_and_cookies('cookie')
 
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas
