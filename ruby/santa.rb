b8a61dddbdaa05e6584a6f478bdfca5ac6c916e8
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-11 06:17:34 -0700

    Added attributes to #initialize

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,31 @@
 class Santa
 	
 	def speak
-		puts 'Ho, ho, ho! Haaaappy holidays!'	
+		puts 'Ho, ho, ho! Haaaappy holidays!'
+			
 	end
 
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
+
 	end
 
-	def initialize
+	def initialize(gender, ethnicity)
+		
+		@gender = gender
+		@ethnicity = ethnicity
+
 		puts "Initializing Santa instance..."
+		reindeer_ranking = [
+			                "Rudolph", "Dasher", 
+			                "Dancer", "Prancer", 
+			                "Vixen", "Comet",   
+			                "Cupid", "Donner", 
+			                "Blitzen"
+			                ]
+
+		@age = 0
+
 	end
 
 end
