fb0f9203f87f149fcc4c185e89cc4c185d5c3130
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-16 02:25:01 -0700

    Add pseudocode to file

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+=begin
+	-Create Pokemon Class
+		-Create three attributes:
+			1. Pokemon Type
+			2. Pokemon Level
+			3. Pokemon Name
+			4. Experience 
+		-Create three methods:
+			1. Level up
+			2. Pokemon cry
+			3. Pokemon attack
+			4. Gain Experience
+			5. Pokemon Evolve
+=end
