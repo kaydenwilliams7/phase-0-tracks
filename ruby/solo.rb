cb27f28422e3fe56f068868cb066a40cc1130100
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-16 02:43:45 -0700

    Edit pseudocode and build class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,23 +3,36 @@
 		-Create three attributes:
 			1. Pokemon Type
 			2. Pokemon Level
-			3. Pokemon Name
-			4. Experience points
-			5. Gender
+			3. Experience points
+			4. Gender
 		-Create three methods:
 			1. Level up
 			2. Pokemon cry
 			3. Pokemon attack
-			4. Gain Experience
-			5. Pokemon Evolve
+			4. Pokemon Evolve
 =end
 
 class Pokemon
-	def initialize(gender)
+	def initialize(type, level, name, gender)
+		@type = type
+		@level = level
+		@name = name
 		@gender = gender
 		puts "\n"
-		puts "Creating new pokemon..." 
+		puts "Creating new pokemon..."
+		puts "#{name} is a #{gender} level #{level} #{type} type pokemon!" 
 
 	end
 
+
+
 end
+
+pikachu = Pokemon.new("electric", 5, "Pikachu", "female")
+bulbasaur = Pokemon.new("grass", 5, "Bulbasaur", "male")
+
+
+
+
+
+
