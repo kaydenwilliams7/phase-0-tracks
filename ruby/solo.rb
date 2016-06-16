0bd05dec5e0a39def5746130c42d15966dd1e154
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-16 04:14:48 -0700

    Add #pokemon_creator

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,8 +3,7 @@
 		-Create three attributes:
 			1. Pokemon Type
 			2. Pokemon Level
-			3. Experience points
-			4. Gender
+			3. Gender
 		-Create three methods:
 			1. Level up
 			2. Pokemon attack
@@ -13,9 +12,9 @@
 
 class Pokemon
 	attr_reader :name, :gender
-	attr_writer :type, :level
+	attr_accessor :type, :level
 
-	def initialize(type, name, gender, level)
+	def initialize(name, type, gender, level)
 		@type = type
 		@name = name
 		@gender = gender
@@ -32,23 +31,23 @@ class Pokemon
 		puts "#{@name} is now level #{@level}!"
 	end
 
-	def evolve(name) 
-		if name == "Pikachu"
+	def evolve 
+		if @name == "Pikachu"
 			puts "\n"
 			puts "#{name} evolved into Raichu!"
 		    @name = "Raichu"
 		
-		elsif name == "Bulbasaur"
+		elsif @name == "Bulbasaur"
 		    	puts "\n"
 		    	puts "#{name} evolved into Ivysaur!"
 		    	@name = "Ivysaur"
 		
-		elsif name == "Charmander"
+		elsif @name == "Charmander"
 			puts "\n"
 			puts "#{name} evolved into Charmeleon!"
 			@name = "Charmeleon"
 
-		elsif name == "Squirtle"
+		elsif @name == "Squirtle"
 				puts "\n"
 				puts "#{name} evolved into Wartortle!"
 				@name = "Wartortle"
@@ -64,26 +63,56 @@ class Pokemon
 		puts "#{@name} attacked the enemy Pokemon!"
 	end
 
-
 end
 
-pikachu = Pokemon.new("electric", "Pikachu", "female", 5)
-bulbasaur = Pokemon.new("grass", "Bulbasaur", "male", 5)
-charmander = Pokemon.new("fire", "Charmander", "female", 5)
-squirtle = Pokemon.new("water", "Squirtle", "male", 5)
+pikachu = Pokemon.new("Pikachu", "electric", "female", 5)
+bulbasaur = Pokemon.new("Bulbasaur", "grass", "male", 5)
+charmander = Pokemon.new("Charmander", "fire", "female", 5)
+squirtle = Pokemon.new("Squirtle", "water", "male", 5)
 
 charmander.level_up(1)
-charmander.evolve("Charmander")
+charmander.evolve
 puts charmander.name
 
-squirtle.evolve("Squirtle")
+squirtle.evolve
 puts squirtle.name
 
 
 puts bulbasaur.attack
 
+def pokemon_creator
+	created_pokemon_array = []
 
+	user_response = ""
+	until user_response == "no"
+		puts "What pokemon would you like to create?"
+		new_pokemon = gets.chomp
+		
+		puts "\n"
+		puts "What is the pokemon's type?"
+		new_type = gets.chomp
+	
+		puts "\n"
+		puts "What is the new pokemon's level?"
+		new_level = gets.chomp.to_i
+	
+		puts "\n"
+		puts "What gender is the new pokemon?"
+		new_gender = gets.chomp
+		
+		puts "\n"
+		created_pokemon_array.push(pokemon = Pokemon.new(new_pokemon, new_type, new_gender, new_level))
+		p created_pokemon_array
+		
+		puts "\n"
+		puts "Create another new pokemon?"
+		user_response = gets.chomp
+	end
 
+	puts "Thanks for using the pokemon creator! Have a great day!"
+end
+
+pokemon_creator
 
 
 
