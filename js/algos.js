130b7291fc85cdef6c84bfcea21aeba317b45b51
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-19 00:41:41 -0700

    Add pseudocode for solution to longestPharse problem

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+/* Create the shell of a function and name it longestPharse
+longestPharse takes in an array of strings and
+returns the longest pharse in the array.
+-Set a variable called currentLongestPharse equal to "" outside
+ loop
+-Set up a for loop that iterates through the strArray and has
+ an if statement that updates currentLongestPharse to the value
+ of the index if it is longer than the current length of
+ currentLongestPharse
+-set a variable called finalPharse equal to currentLongestPharse
+ after the loop.
+-Return finalPharse.
