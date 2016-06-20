497f85f81dac23a46fa3409a141f82dcc82153ba
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-06-19 20:44:26 -0700

    Add colors and horseNames arrays

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["red", "blue", "yellow", "green"];
+var horseNames = ["dom", "Kayden", "Amy", "Mike"];
+
+colors.push("white");
+console.log(colors);
+
+horseNames.push("Scooby");
+console.log(horseNames);
