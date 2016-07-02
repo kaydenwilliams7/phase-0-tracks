9efe446eb1efbff23dbbdd1fc6027d7c3c8088b7
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-07-01 17:34:23 -0700

    Add function

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,9 @@
 console.log("This script is linked properly!");
-document
 var a = document.getElementsByTagName('img');
 b = a[0];
-b.style.border= "5px dashed black";
+// b.style.border= "5px dashed black";
+
+function addBlackBorder(event) {
+  event.target.style.border= "5px dashed black";
+}
+b.addEventListener("click", addBlackBorder);
